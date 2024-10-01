127d77eb75b32f65e3ea42b6d3d467e5c85e4d24
Vladislav Elcheninov <133252067+vvladislovv@users.noreply.github.com>
2024-10-01 19:04:16 +0300

    Add files via upload

---
+++ bro-birthsday-bot/db/db_impl.py
@@ -0,0 +1,94 @@
+import os
+
+import psycopg2
+from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from settings import DATABASE_URL
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
+logger = logging.getLogger(__name__)
+
+
+def conn():
+    try:
+        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = connection.cursor()
+        logger.info("Открыто соедениние с PostgeSQL")
+    except (Exception, Error) as error:
+        cursor = None
+        logger.error(f"Ошибка при работе с PostgreSQL {error}")
+        raise Error
+
+    return cursor, connection
+
+
+def close_conn(cursor, connection):
+    cursor.close()
+    connection.close()
+    logger.info("Соединение с PostgreSQL закрыто")
+
+
+def get_members_for_chat(chat_id):
+    query = f"SELECT snapshot FROM DATA WHERE chat_id LIKE '{chat_id}'"
+    cursor = conn()
+    cursor[0].execute(query)
+    snapshot = cursor[0].fetchall()
+    cursor[0].close()
+    close_conn(cursor[0], cursor[1])
+    return snapshot
+
+
+def get_members_and_chats_id():
+    query = f"SELECT snapshot, chat_id FROM DATA"
+    cursor = conn()
+    cursor[0].execute(query)
+    data = cursor[0].fetchall()
+    cursor[0].close()
+    close_conn(cursor[0], cursor[1])
+    return data
+
+
+def set_members_for_chat(chat_id, snapshot):
+    query = f"INSERT INTO DATA (snapshot, chat_id) VALUES('{snapshot}', {chat_id})"
+    cursor = conn()
+    cursor[0].execute(query)
+    cursor[0].close()
+    close_conn(cursor[0], cursor[1])
+
+
+def get_all_chat_id():
+    query = f"SELECT DISTINCT chat_id FROM DATA"
+    cursor = conn()
+    cursor[0].execute(query)
+    chat_id_list = cursor[0].fetchall()
+    cursor[0].close()
+    close_conn(cursor[0], cursor[1])
+    return chat_id_list
+
+
+def get_changelog():
+    query = f"SELECT change_log FROM UPDATES WHERE notified IS false"
+    cursor = conn()
+    cursor[0].execute(query)
+    changelog = cursor[0].fetchall()
+    cursor[0].close()
+    close_conn(cursor[0], cursor[1])
+    return changelog
+
+
+def delete_member_from_db(chat_id, name):
+    query = f"DELETE FROM data WHERE chat_id LIKE '{chat_id}' AND snapshot LIKE '{name}%'"
+    cursor = conn()
+    cursor[0].execute(query)
+    cursor[0].close()
+    close_conn(cursor[0], cursor[1])
+
+
+def update_notified():
+    query = f"UPDATE UPDATES SET notified=true WHERE notified != true"
+    cursor = conn()
+    cursor[0].execute(query)
+    cursor[0].close()
+    close_conn(cursor[0], cursor[1])
